9e9327ace9004edc291e1da00fdc198a81b9a104
r-mhjn <rachitmahajan9936@gmail.com>
2020-01-22 21:20:00 +0530

    minor changes

---
+++ apiKey.js
@@ -0,0 +1,2 @@
+const apiKey = "428b86f03e7d0dd91202de9909375521";
+export default apiKey;
---
+++ weatherConditions.js
@@ -0,0 +1,51 @@
+module.exports = weatherConditions = {
+	Rain: {
+		color: '#005BEA',
+		title: 'Raining',
+		subtitle: 'Get a cup of coffee',
+		icon: 'weather-rainy',
+	},
+	Clear: {
+		color: '#f7b733',
+		title: 'So Sunny',
+		subtitle: 'Weather is calm and sunny',
+		icon: 'weather-sunny',
+	},
+	Thunderstorm: {
+		color: '#616161',
+		title: 'A Storm is coming',
+		subtitle: 'Storm\'s coming!',
+		icon: 'weather-lightning',
+	},
+	Clouds: {
+		color: '#1F1C2C',
+		title: 'Clouds',
+		subtitle: 'Clouds ahead!',
+		icon: 'weather-cloudy',
+	},
+
+	Snow: {
+		color: '#00d2ff',
+		title: 'Snow',
+		subtitle: 'Let\'s make a Snowman',
+		icon: 'weather-snowy',
+	},
+	Drizzle: {
+		color: '#076585',
+		title: 'Drizzle',
+		subtitle: 'Drizzle Happiness',
+		icon: 'weather-hail',
+	},
+	Haze: {
+		color: '#66A6FF',
+		title: 'Haze',
+		subtitle: 'It\'s haze-y! Drive Safe!',
+		icon: 'weather-hail',
+	},
+	Mist: {
+		color: '#3CD3AD',
+		title: 'Mist',
+		subtitle: "Get Mistified!",
+		icon: 'weather-fog',
+	},
+};
